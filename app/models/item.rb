59e3cff31167118cded1add4e4ff0aaafb17000e
Fredis <fredericstojicevic@gmail.com>
2018-11-26 14:40:26 +0100

    Add validations to Item model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+	validates :title, :description, :price, :image_url, presence: true
+	validates :price, numericality: { greater_than: 0, message: "The value has to be greater than 0." }
 end
