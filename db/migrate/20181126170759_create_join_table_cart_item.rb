6994ad8bd1d6c015c480b6993722da722320c8af
Fredis <fredericstojicevic@gmail.com>
2018-11-27 10:55:37 +0100

    Add the migration for the join table between items and cart.

---
+++ db/migrate/20181126170759_create_join_table_cart_item.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableCartItem < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :carts, :items do |t|
+      # t.index [:cart_id, :item_id]
+      # t.index [:item_id, :cart_id]
+    end
+  end
+end
