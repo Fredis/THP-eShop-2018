3beb7a3c4caa2038fc445b6457e72202ce85a7cd
Cedric Soares <cedricsoares@me.com>
2018-11-27 17:24:42 +0100

    Add relations between Orders and Items tabs, Orders and Users tabs in related models

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
 	validates :title, :description, :price, :image_url, presence: true
 	validates :price, numericality: { greater_than: 0, message: "The value has to be greater than 0." }
 	has_and_belongs_to_many :carts
+	has_and_belongs_to_many :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 has_one :cart
+has_many :orders
 end
