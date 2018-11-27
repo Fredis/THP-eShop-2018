2160bf22c98acaef75e811b63d673576808e297e
MelloFlo <florian_pinto@outlook.fr>
2018-11-27 16:10:47 +0100

    modif index, show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+  	@items = Item.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-	
+
 resources :items, only: [:show, :index]
 
  devise_for :views
  devise_for :users
- get 'home/index'
- root 'home#index'
+ root 'home#index', :controller => "items", :action => "index"
 
 end
