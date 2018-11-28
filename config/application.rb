56b8f5ced56309431d06dcb6d10acdcafc5ea814
MarcelALng <marcel.nguyen@gmail.com>
2018-11-28 11:07:02 +0100

    add credentials

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module ThpEshop2018
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_mailer.default_url_options = {
+
+    	host: 'http://www.gmail.com'
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
