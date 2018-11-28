e48602474ec778e9e4dead275dc8befba2a7c45a
MelloFlo <florian_pinto@outlook.fr>
2018-11-28 19:08:03 +0100

    add transfer card in order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,23 @@
 class OrdersController < ApplicationController
 
 	def index
-	@cart = Cart.find(session[:cart_id])
-  	@items = @cart.items
+	  @cart = Cart.find(session[:cart_id])
+  	  @items = @cart.items
 	end
 
-	def show
+
+
+	def create
+	  @items = Cart.find(session[:cart_id]).items
+		puts '////////////////'
+		puts @items
+	  @order = Order.create(user_id: current_user.id, status: "waiting for payment")
+	  @items.each do |element|
+		@order.items << element
+	  end
+
+	 puts @order.items
+	
 	end
 
 end
