f11c72a3eebc5054f0f3e3461342775aa21adb5d
Cedric Soares <cedricsoares@me.com>
2018-11-28 19:32:33 +0100

    Add item_spec.rb for RSpec tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	Rspec.describe Item, type: :model do 
+		subject { described_class.new(title: "Anything", description: "Anything",
+	 		 	price: "12", image_url: "imageid.com," )
+	 	}
+
+ 		context "When a new item is created by admin" do 
+
+		 	it "Is valid with valid attributes" do 
+		 		expect(subject).to be_valid
+		 	end
+
+		 	It "Is not valid without a title" do
+			  	subject.title = nil 
+			  	expect(subject).to not_be_valid
+		  	end
+
+		 	it "Is not valid without a description" do 
+		  		subject.description = nil 
+		  		expect(subject).to not_be_valid 
+		  	end  
+
+		  	it "Is not valid without a price" do 
+		  		subject.price = nil
+		  		expect(subject).to not_be_valid 
+			end
+
+			it "Is not valid with a price equal under $1 " 
+				subject.price < 1
+				expect(subject).to not_be_valid
+			end 
+		end
+	end	
 end
