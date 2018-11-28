260c03f352f073dcbf8b3197a54c8e0879587ddc
Fredis <fredericstojicevic@gmail.com>
2018-11-28 17:27:57 +0100

    The user can add an item from an item page.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,14 @@ class ItemsController < ApplicationController
   end
 
   def add_to_cart
-  	cart = Cart.find(session[:cart_id])
-		puts "yoooooooooo"
+  	@cart = Cart.find(session[:cart_id])
   	puts params[:id]
-  	cart.items << Item.find(params[:id])
+  	@cart.items << Item.find(params[:id])
+
+    respond_to do |format|
+       format.html { redirect_to email_path(params[:id]) }
+       format.js
+    end
   end
 
 end
