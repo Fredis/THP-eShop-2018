89ae32a0c7e1c2b914aa0a3fd933b593a377b52e
Fredis <fredericstojicevic@gmail.com>
2018-11-29 23:24:21 +0100

    Working on routes renaming.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ get "paymenthistory", to: "orders#payment_history"
 post "order_validation", to: "orders#order_validation"
 
 resources :items, only: [:show, :index]
-resources :carts, only: [:show, :new, :create, :destroy]
+resources :carts, only: [:show, :new, :create, :destroy], path:'', path_names: {carts: 'carts'}
 resources :orders
 resources :charges
 
 devise_for :views
-devise_for :users
+devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 root 'home#index' #, :controller => "items", :action => "index"
 
 end
