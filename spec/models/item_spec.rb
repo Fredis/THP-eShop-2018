c845040738298593103287759bafed60e70b96cd
Cedric Soares <cedricsoares@me.com>
2018-11-29 01:15:29 +0100

    item_spec.rb updated to support shoulda syntax

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,38 +7,36 @@ RSpec.describe Item, type: :model do
 		price: 12, image_url: "imageid.com" )
 	}
 
-		context "When a new item is created by admin" do
+	context "When a new item is created by admin" do
 
 		it "Is valid with valid attributes" do
 			expect(subject).to be_valid
 		end
 
-		it "Is not valid without a title" do
-			subject.title = nil
-			expect(subject).to_not be_valid
-		end
 
-		#it "Is not valid without a description" do
-			#subject.description = nil
-			#expect(subject).to_not be_valid
+		it { should validate_presence_of(:title) }
+
+
 		it { should validate_presence_of(:description) }
 
-		#end  
 
-		it "Is not valid without a price" do
-			subject.price = nil
-			expect(subject).to_not be_valid
+		it { should validate_presence_of(:price)}
+
+
+		it do
+			should_not allow_value(0).
+				for(:price)
 		end
 
-		it "Is not valid with a price under $1 " do
-			expect(subject.price).to_not eql(0)
-		end 
-
-		it "Is not valid without a image_url" do
-			subject.image_url = nil
-			expect(subject).to_not be_valid
-		end
-
+		it { should validate_presence_of(:image_url) }
 	end
+
+	context "Associations" do
+		
+		it { should have_and_belong_to_many(:carts) }
+
+		it { should have_and_belong_to_many(:orders) }
+	end
+
 end
 
