9237e119e94983faa20575742f3a24428ab218f3
Cedric Soares <cedricsoares@me.com>
2018-11-29 10:36:42 +0100

    Add order_spec.rb RSpec test in spec/models

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Order, type: :model do
+	subject {
+		described_class.new(user_id: 12)
+	}
+
+	context "When a new cart is created" do
+		
+		it { should validate_presence_of(:user_id) }
+
+		it do
+			should have_db_column(:user_id).of_type(:integer)
+		end
+
+		it { should have_db_index(:user_id) }
+
+	end
+
+	context "Associatons" do
+
+		it { should belong_to(:user) }
+
+		it { should have_and_belong_to_many(:items) }
+	end
+
+
+end
+
+
+
+
