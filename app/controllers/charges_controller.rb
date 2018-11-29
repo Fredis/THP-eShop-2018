104e17986192ee9c7581ec50af9a98aad229be0d
MelloFlo <florian_pinto@outlook.fr>
2018-11-29 19:01:29 +0100

    integration of stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,12 +1,24 @@
 class ChargesController < ApplicationController
 	
 	def new
+		  @order = Order.create(user_id: current_user.id, status: "Payment done.")
+		  puts "yooooooooo"
+		  puts @order.errors.messages
+		  @items.each do |element|
+			@order.items << element
+		  end
+		 @items.destroy
+		 Cart.find(session[:cart_id]).destroy
+		 session.delete(:cart_id)
 	end
 
 	def create
-		# Amount in cents
-		@amount = 500
 
+	  @items = Cart.find(session[:cart_id]).items
+
+
+		@amount = (@items.sum(:price)*100).to_i
+		puts @amount
 		customer = Stripe::Customer.create(
 		:email => params[:stripeEmail],
 		:source  => params[:stripeToken]
@@ -21,6 +33,11 @@ class ChargesController < ApplicationController
 
 		rescue Stripe::CardError => e
 		flash[:error] = e.message
+
+
+
 		redirect_to new_charge_path
+
+
 	end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,16 +8,7 @@ class OrdersController < ApplicationController
 
 
 	def create
-	  @items = Cart.find(session[:cart_id]).items
-	  @order = Order.create(user_id: current_user.id, status: "waiting for payment")
-	  puts "yooooooooo"
-	  puts @order.errors.messages
-	  @items.each do |element|
-		@order.items << element
-	  end
-	 @items.destroy
-	 Cart.find(session[:cart_id]).destroy
-	 session.delete(:cart_id)
+
 	end
 
 	def payment_history
