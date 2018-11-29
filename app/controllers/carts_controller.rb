c0f96082a757457d51014557e62e3f07edd3cdac
MelloFlo <florian_pinto@outlook.fr>
2018-11-29 13:02:49 +0100

    add obtion remove items of cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,7 +6,16 @@ class CartsController < ApplicationController
   end
 
   def destroy
-    
+    @cart = Cart.find(session[:cart_id])
+    @items = @cart.items
+
+    puts params[:id]
+    @cart.items.delete(@cart.items.find(params[:id]))
+
+    respond_to do |format|
+       format.html { redirect_to cart_path(params[:id]) }
+       format.js
+    end
   end
 
   def new
