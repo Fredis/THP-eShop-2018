abf04bcd5e3f97de38a214ac593fee1a2cddd7af
MelloFlo <florian_pinto@outlook.fr>
2018-11-29 15:44:29 +0100

    bug ajout plusieurs item corrigé

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,12 +12,24 @@ class ItemsController < ApplicationController
   def add_to_cart
 
   	@cart = Cart.find(session[:cart_id])
-  	puts params[:id]
-    
-    if @cart.items.find(params[:id])''
+
+    if @cart.items.count != 0
+      i=0
+
+      @cart.items.each do |item|
+        if params[:id].to_i == item.id
+          i=1
+        end
+      end
+      
+      if i != 1
+        @cart.items << Item.find(params[:id])
+      end
+
+    else
       @cart.items << Item.find(params[:id])
     end
-    
+
     respond_to do |format|
        format.html { redirect_to items_path(params[:id]) }
        format.js
