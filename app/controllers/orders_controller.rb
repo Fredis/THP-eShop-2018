074b4f41a63ffc27a791d645e9aecf2dbed16e35
Snakultime <snakultime@hotmail.fr>
2018-11-29 15:49:05 +0100

    update Stripe but button not working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,39 +1,45 @@
 class OrdersController < ApplicationController
 
 	def index
-	  @cart = Cart.find(session[:cart_id])
-  	  @items = @cart.items
+		@cart = Cart.find(session[:cart_id])
+		@items = @cart.items
 	end
 
 
 
 	def create
-	  @items = Cart.find(session[:cart_id]).items
-	  @order = Order.create(user_id: current_user.id, status: "waiting for payment")
-	  puts "yooooooooo"
-	  puts @order.errors.messages
-	  @items.each do |element|
-		@order.items << element
-	  end
+		@items = Cart.find(session[:cart_id]).items
+		@order = Order.create(user_id: current_user.id, status: "waiting for payment")
+		puts "yooooooooo"
+		puts @order.errors.messages
+		@items.each do |element|
+			@order.items << element
+		end
 		@items.destroy
- 	 Cart.find(session[:cart_id]).destroy
- 	 session.delete(:cart_id)
+		Cart.find(session[:cart_id]).destroy
+		session.delete(:cart_id)
 
-	 @amount = @items.sum(:price)
+		@amount = @items.sum(:price)
 
-	 customer = Stripe::Customer.create(
-		 :email => params[:stripeEmail],
-		 :source  => params[:stripeToken]
-	 )
-	 charge = Stripe::Charge.create(
-		 :customer    => customer.id,
-		 :amount      => @amount,
-		 :description => 'Kitten\'s payment',
-		 :currency    => 'us'
-	 )
- rescue Stripe::CardError => e
-	 flash[:error] = e.message
-	 redirect_to root_path
+		customer = Stripe::Customer.create(
+			:email => params[:stripeEmail],
+			:source  => params[:stripeToken]
+		)
+		charge = Stripe::Charge.create(
+			:customer    => customer.id,
+			:amount      => @amount,
+			:description => 'Kitten\'s payment',
+			:currency    => 'usd'
+		)
+			redirect_to root_path
+
+	rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to @order
+	end
+
+	def update
+		@order = Order.find(params[:id])
 	end
 
 end
